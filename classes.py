f9435a7a2dfb20a4632349e336696519a692aa4d
dilpreetC <dilpreet_chana@hotmail.com>
2015-02-17 14:54:46 -0500

    Added move AI for enemy

--- classes.py
+++ classes.py
@@ -1,11 +1,13 @@
 # classes.py
 
 from pygame import *
+from enum import *
 
 class player:
-	def __init__(self, speed, tongue_range, tongue_speed, shoot_range, is_loaded, x, y):
+	def __init__(self, speed, tongue_range, tongue_speed, shoot_range, is_loaded, x, y, moveMode):
 		self.x = x
 		self.y = y
+		self.moveMode = moveMode
 		self.playerRect = Rect(x, y, 20, 50)
 		self.speed = speed
 		self.tongue_range = tongue_range
@@ -15,17 +17,28 @@ class player:
 
 	def move(self, pressed, screen):
 		"Move player"
-		if pressed[K_UP]:
-			self.y -= self.speed
-		elif pressed[K_DOWN]:
-			self.y += self.speed
-		elif pressed[K_LEFT]:
-			self.x -= self.speed
-		elif pressed[K_RIGHT]:
-			self.x += self.speed
+		if self.moveMode == "arrow":  # temporary use enumerations
+			if pressed[K_UP]:
+				self.y -= self.speed
+			elif pressed[K_DOWN]:
+				self.y += self.speed
+			elif pressed[K_LEFT]:
+				self.x -= self.speed
+			elif pressed[K_RIGHT]:
+				self.x += self.speed
+
+		if self.moveMode == "wasd":
+			if pressed[K_w]:
+				self.y -= self.speed
+			elif pressed[K_s]:
+				self.y += self.speed
+			elif pressed[K_a]:
+				self.x -= self.speed
+			elif pressed[K_d]:
+				self.x += self.speed
 
 		self.playerRect = Rect(self.x, self.y, 20, 50)
-		draw.rect(screen, (255, 255, 255), self.playerRect)
+		draw.rect(screen, (0, 255, 0), self.playerRect)
 
 	def suck(self):
 		"Use tongue to obtain block"
@@ -39,22 +52,51 @@ class player:
 		return self.speed
 	def tongueSpeed(self):
 		"get spped of tongue"
+		return self.tongue_speed
+	def getX(self):
+		"get x position of object"
+		return self.x
+	def getY(self):
+		"get y position of object"
+		return self.y
 
 class enemy:
-	def __init__(self, speed, ko_time, AI_level, kind):
+	def __init__(self, speed, ko_time, AI_level, kind, x, y):
+		self.x = x
+		self.y = y
+		self.enemyRect = Rect(x, y, 20, 50)
 		self.speed = speed
 		self.ko_time = ko_time
 		self.AI_level = AI_level
 		self.kind = kind
 
-	def move(self):
+	def move(self, px, py, screen):
 		"Move enemy"
+		if px > self.x:
+			self.x += self.speed
+		elif px < self.x:
+			self.x -= self.speed
+		elif py > self.y:
+			self.y += self.speed
+		elif py < self.y:
+			self.y -= self.speed
+
+		self.enemyRect = Rect(self.x, self.y, 20, 50)
+		draw.rect(screen, (255, 0, 0), self.enemyRect)
 
 	def knock_out(self):
 		"Enemy is knocked out"
 
 	def AI(self):
 		"AI for enemies"
+
+	# get methods
+	def getX(self):
+		"get x position of object"
+		return self.x
+	def getY(self):
+		"get y position of object"
+		return self.y
 
 class block:
 	def __init__(self, speed, can_pull, kind, can_break):
@@ -68,3 +110,8 @@ class block:
 
 	def throw(self):
 		"Change how block moves while moving"
+
+# enum
+class playerMode(Enum):
+	player_1 = True
+	player_2 = True
--- main.py
+++ main.py
@@ -2,11 +2,20 @@
 
 from pygame import *
 from classes import *
+from enum import *
+from math import *
 
 screen = display.set_mode((700, 500))
 screen.fill((0, 0, 255))
 
-player1 = player(1.6, 100, 3.2, 400, False, 100, 300)
+enemyList = []
+playerList = []
+
+playerList.append(player(3, 100, 3.2, 400, False, 100, 300, "arrow"))
+playerList.append(player(2, 100, 3.2, 400, False, 600, 200, "wasd"))
+
+enemyList.append(enemy(1, 3, 10, "blob", 10, 20))
+enemyList.append(enemy(0.5, 3, 10, "blob", 100, 200))
 
 gameClock = time.Clock()
 running = True
@@ -21,6 +30,18 @@ while running:
 
 	# do player meathods
 	player1.move(pressed, screen)
+	player2.move(pressed, screen)
+
+	# do enemy methods
+	if sqrt((player1.getX()-enemy1.getX())**2 + (player1.getY() - enemy1.getY())**2) < sqrt((player2.getX()-enemy1.getX())**2 + (player2.getY() - enemy1.getY())**2):
+		enemy1.move(player1.getX(), player1.getY(), screen)
+	elif sqrt((player1.getX()-enemy1.getX())**2 + (player1.getY() - enemy1.getY())**2) > sqrt((player2.getX()-enemy1.getX())**2 + (player2.getY() - enemy1.getY())**2):
+		enemy1.move(player2.getX(), player2.getY(), screen)
+
+	if sqrt((player1.getX()-enemy2.getX())**2 + (player1.getY() - enemy2.getY())**2) < sqrt((player2.getX()-enemy2.getX())**2 + (player2.getY() - enemy2.getY())**2):
+		enemy2.move(player1.getX(), player1.getY(), screen)
+	elif sqrt((player1.getX()-enemy2.getX())**2 + (player1.getY() - enemy2.getY())**2) > sqrt((player2.getX()-enemy2.getX())**2 + (player2.getY() - enemy2.getY())**2):
+		enemy2.move(player2.getX(), player2.getY(), screen)
 
 	gameClock.tick(60)
 	display.flip()
